1742de2b6a43978e7fd4914c598036ab0bd3adc3
Flautisimo <flautisimo@gmail.com>
2021-04-25 11:33:38 +0300

    Updates

---
+++ precache-manifest.2abd918d222e42748f50c7f04dca1457.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3604ee2f3cce260c2650",
+    "url": "/portfolio/static/css/main.0c116833.chunk.css"
+  },
+  {
+    "revision": "3604ee2f3cce260c2650",
+    "url": "/portfolio/static/js/main.32dc806d.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  },
+  {
+    "revision": "df64709930cf636c5a18",
+    "url": "/portfolio/static/js/2.5e543551.chunk.js"
+  },
+  {
+    "revision": "f30225a711ed89f452b8778cf7726afe",
+    "url": "/portfolio/index.html"
+  }
+];
